5135102249ceda83e7c2c136f0315c6438c8b7d8
palrit <ritpal8@gmail.com>
2020-07-20 17:28:20 -0400

    Fix changes to merge

---
+++ CrisisApplication/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrisisApplication.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ViewResult SignIn()
+        {
+            return View();
+        }
+
+        public ViewResult SignUp()
+        {
+            return View();
+        }
+    }
+}
