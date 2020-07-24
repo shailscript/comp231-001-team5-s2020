56f69e339640a9bb90e4f47b12bd79d0b33779c8
palrit <ritpal8@gmail.com>
2020-07-24 15:46:00 -0400

    Added 14 files, changes related to signin and signup back up  App Identity, seed data for identity

---
+++ CrisisApplication/Models/IdentitySeedData.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrisisApplication.Models
+{
+    public class IdentitySeedData
+    {
+        private const string adminUser = "Admin";
+        private const string adminPassword = "Secret123$";
+        public static async void EnsurePopulated(IApplicationBuilder app)
+        {
+            AppIdentityDbContext context = app.ApplicationServices
+            .GetRequiredService<AppIdentityDbContext>();
+            context.Database.EnsureDeleted();
+            context.Database.Migrate();
+
+            UserManager<IdentityUser> userManager = app.ApplicationServices
+            .GetRequiredService<UserManager<IdentityUser>>();
+            IdentityUser user = await userManager.FindByIdAsync(adminUser);
+            if (user == null)
+            {
+                user = new IdentityUser("Admin");
+                await userManager.CreateAsync(user, adminPassword);
+            }
+        }
+    }
+}
---
+++ CrisisApplication/Models/ViewsModel/SignupModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrisisApplication.Models.ViewsModels
+{
+    public class SignupModel
+    {
+
+        public int UserID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Please enter Confirm password")]
+        [Compare("Password", ErrorMessage = "Password and confirm password should be matched")]
+        public string ConfirmPassword { get; set; }
+
+        public Boolean isAdmin;
+
+        [Required(ErrorMessage = "Please enter Email")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Please enter a valid email address")]
+        public string Email { get; set; }
+
+        public string ReturnUrl { get; set; } = "/";
+    }
+}
