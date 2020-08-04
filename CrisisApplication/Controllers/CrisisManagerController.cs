676659fcef50a45167c58520d3be7d81901dab2b
romisyed <60547714+romisyed@users.noreply.github.com>
2020-08-04 13:01:53 -0400

    Add Crisis Manager Controller (#7)

---
+++ CrisisApplication/Controllers/CrisisManagerController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrisisApplication.Controllers
+{
+    public class CrisisManagerController : Controller
+    {
+        public ViewResult CrisisManagerHome()
+        {
+            return View();
+        }
+
+        public ViewResult ViewContacts()
+        {
+            return View();
+        }
+        public ViewResult ViewStatus()
+        {
+            return View();
+        }
+
+        public ViewResult Alert()
+        {
+            return View();
+        }
+    }
+}
