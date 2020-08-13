4f85d8adfa0d0c319327b04c61b161a5eeb3d922
shamreenshaik <shamreenshaik@gmail.com>
2020-08-13 17:49:36 -0400

    Add Contact UI( Controller, ContactHome, Contactreponseconfrm)

---
+++ CrisisApplication/Controllers/ContactController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrisisApplication.Controllers
+{
+    public class ContactController : Controller
+    {
+        public ViewResult ContactHome()
+        {
+            return View();
+        }
+        public ViewResult Contactresponseconfrm()
+        {
+            return View();
+        }
+    }
+}
