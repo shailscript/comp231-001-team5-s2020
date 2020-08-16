2c66c1a0e158b04c5dc66f1c88da9477a4638da8
WalterCorbitt <46976605+WalterCorbitt@users.noreply.github.com>
2020-08-16 16:53:22 -0400

    Moved email broadcast to Event Controller

---
+++ CrisisApplication/Migrations/20200816194847_RespondentAdded.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CrisisApplication.Migrations
+{
+    public partial class RespondentAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Respondents",
+                columns: table => new
+                {
+                    RespondentID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    StudentID = table.Column<int>(nullable: false),
+                    Email = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Respondents", x => x.RespondentID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Respondents");
+        }
+    }
+}
